9d46d9000eed70f40d57207c384d3a8e54734d22
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2023-09-09 22:17:08 +0500

    updated levels

--- src/components/levels/Levels.jsx
+++ src/components/levels/Levels.jsx
@@ -15,8 +15,13 @@ const Levels = () => {
   const [thirdWithdraw, setThirdWithdraw] = useState(0);
   const [fourthWithdraw, setFourthWithdraw] = useState(0);
   const [fifthWithdraw, setFifthWithdraw] = useState(0);
+  const [first_person_commsion, setFirst_person_commision] = useState(0);
+  const [second_person_commsion, setSecond_person_commision] = useState(0);
+  const [third_person_commsion, setThird_person_commision] = useState(0);
+  const  [fourth_person_commsion, setFourth_person_commision] = useState(0);
+  const [fifth_person_commsion, setFifth_person_commision] = useState(0);
   const [unlock, setUnlock] = useState(0);
-
+const [rank, setRank] = useState("")
   const showModal = () => {
     setIsModalOpen(true);
   };
@@ -28,7 +33,13 @@ const Levels = () => {
     setThirdWithdraw(row.thirdWithdraw);
     setFourthWithdraw(row.fourthWithdraw);
     setFifthWithdraw(row.fifthWithdraw);
+    setFifth_person_commision(row.fifth_person_commision);
+    setFourth_person_commision(row.fourth_person_commision);
+    setThird_person_commision(row.third_person_commision);
+    setSecond_person_commision(row.second_person_commision);
+    setFirst_person_commision(row.first_person_commision);
     setDocid(row.id);
+    setRank (row.rank);
     setIsModalOpenb(true);
   };
 
@@ -44,6 +55,12 @@ const Levels = () => {
     setThirdWithdraw(0);
     setFourthWithdraw(0);
     setFifthWithdraw(0);
+    setFirst_person_commision(0);
+    setSecond_person_commision(0);
+    setThird_person_commision(0);
+    setFourth_person_commision(0);
+    setFifth_person_commision(0);
+    setRank("")
   };
 
   const handleCancel = () => {
@@ -58,6 +75,12 @@ const Levels = () => {
     setThirdWithdraw(0);
     setFourthWithdraw(0);
     setFifthWithdraw(0);
+    setFirst_person_commision(0);
+    setSecond_person_commision(0);
+    setThird_person_commision(0);
+    setFourth_person_commision(0);
+    setFifth_person_commision(0);
+setRank("")
   };
 
   const [data, setData] = useState([]);
@@ -74,6 +97,12 @@ const Levels = () => {
         thirdWithdraw: Number(thirdWithdraw),
         fourthWithdraw: Number(fourthWithdraw),
         fifthWithdraw: Number(fifthWithdraw),
+        first_person_commsion: Number(first_person_commsion),
+        second_person_commsion: Number(second_person_commsion),
+        third_person_commsion: Number(third_person_commsion),
+        fourth_person_commsion: Number(fourth_person_commsion),
+        fifth_person_commsion: Number(fifth_person_commsion),
+        rank:rank 
       });
 
       toast.success("Data saved successfully");
@@ -83,6 +112,12 @@ const Levels = () => {
       setThirdWithdraw(0);
       setFourthWithdraw(0);
       setFifthWithdraw(0);
+      setFirst_person_commision(0);
+      setSecond_person_commision(0);
+      setThird_person_commision(0);
+      setFourth_person_commision(0);
+      setFifth_person_commision(0);
+      setRank("")
     } catch (error) {
       toast.error("Error saving data:", error);
     }
@@ -119,6 +154,12 @@ const Levels = () => {
         thirdWithdraw: Number(thirdWithdraw),
         fourthWithdraw: Number(fourthWithdraw),
         fifthWithdraw: Number(fifthWithdraw),
+        first_person_commsion: Number(first_person_commsion),
+        second_person_commsion: Number(second_person_commsion),
+        third_person_commsion: Number(third_person_commsion),
+        fourth_person_commsion: Number(fourth_person_commsion),
+        fifth_person_commsion: Number(fifth_person_commsion),
+      rank:rank
       });
 
       toast.success("Data updated successfully");
@@ -128,7 +169,13 @@ const Levels = () => {
       setThirdWithdraw(0);
       setFourthWithdraw(0);
       setFifthWithdraw(0);
+      setFirst_person_commision(0);
+      setSecond_person_commision(0);
+      setThird_person_commision(0);
+      setFourth_person_commision(0);
+      setFifth_person_commision(0);
       setDocid("");
+      setRank("")
       setIsModalOpenb(false);
     } catch (error) {
       toast.error("Error updating data:", error);
@@ -188,12 +235,18 @@ const Levels = () => {
   ];
 
   const reorderedUserColumns = [
+    { field: "rank", headerName: "Ranking", width: 70 },
     { field: "unlock", headerName: "Unlock On", width: 80 },
    { field: "firstWithdraw", headerName: "First Withdraw", width: 130 },
     { field: "secondWithdraw", headerName: "Second Withdraw", width: 130 },
     { field: "thirdWithdraw", headerName: "Third Withdraw", width: 130 },
     { field: "fourthWithdraw", headerName: "Fourth Withdraw", width: 130 },
     { field: "fifthWithdraw", headerName: "Fifth Withdraw", width: 130 },
+    { field: "first_person_commsion", headerName: "1st Person Commission", width: 130 },
+    { field: "second_person_commsion", headerName: "2nd Person Commission", width: 130 },
+    { field: "third_person_commsion", headerName: "3rd Person Commission", width: 130 },
+    { field: "fourth_person_commsion", headerName: "4th Person Commission", width: 130 },
+    { field: "fifth_person_commsion", headerName: "5th Person Commission", width: 130 },
   ];
 
   const allColumns = [...reorderedUserColumns, ...actionColumn];
@@ -214,7 +267,6 @@ const Levels = () => {
         >
           Add LEVEL
         </button>
-        <p className="link">Add New</p>
       </div>
       <DataGrid
         className="datagrid"
@@ -230,6 +282,16 @@ const Levels = () => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
+      
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="rank">Rank:</label>
+          <input
+            type="text"
+            id="rank"
+            value={rank}
+            onChange={(e) => setRank(e.target.value)}
+          />
+        </div>
         <div style={{ marginBottom: "16px" }}>
           <label htmlFor="unlock">unlock:</label>
           <input
@@ -282,6 +344,54 @@ const Levels = () => {
             id="fifthWithdraw"
             value={fifthWithdraw}
             onChange={(e) => setFifthWithdraw(e.target.value)}
+          />
+        </div>
+         <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="first_person_commsion">1st Person Commission:</label>
+          <input
+            type="number"
+            id="first_person_commsion"
+            value={first_person_commsion}
+            onChange={(e) => setFirst_person_commision(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="second_person_commsion">2nd Person Commission:</label>
+          <input
+
+            type="number"
+            id="second_person_commsion"
+            value={second_person_commsion}
+            onChange={(e) => setSecond_person_commision(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="third_person_commsion">3rd Person Commission:</label>
+          <input
+
+            type="number"
+            id="third_person_commsion"
+            value={third_person_commsion}
+            onChange={(e) => setThird_person_commision(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="fourth_person_commsion">4th Person Commission:</label>
+          <input
+
+            type="number"
+            id="fourth_person_commsion"
+            value={fourth_person_commsion}
+            onChange={(e) => setFourth_person_commision(e.target.value)}
+          />
+        </div> <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="fifth_person_commsion">5th Person Commission:</label>
+          <input
+
+            type="number"
+            id="fifth_person_commsion"
+            value={fifth_person_commsion}
+            onChange={(e) => setFifth_person_commision(e.target.value)}
           />
         </div>
         <button
@@ -297,6 +407,15 @@ const Levels = () => {
         onOk={handleOkb}
         onCancel={handleCancelb}
       >
+         <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="rank">Rank:</label>
+          <input
+            type="text"
+            id="rank"
+            value={rank}
+            onChange={(e) => setRank(e.target.value)}
+          />
+        </div>
         <div style={{ marginBottom: "16px" }}>
           <label htmlFor="unlock">unlock:</label>
           <input
@@ -349,6 +468,54 @@ const Levels = () => {
             id="fifthWithdraw"
             value={fifthWithdraw}
             onChange={(e) => setFifthWithdraw(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="first_person_commsion">1st Person Commission:</label>
+          <input
+            type="number"
+            id="first_person_commsion"
+            value={first_person_commsion}
+            onChange={(e) => setFirst_person_commision(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="second_person_commsion">2nd Person Commission:</label>
+          <input
+
+            type="number"
+            id="second_person_commsion"
+            value={second_person_commsion}
+            onChange={(e) => setSecond_person_commision(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="third_person_commsion">3rd Person Commission:</label>
+          <input
+
+            type="number"
+            id="third_person_commsion"
+            value={third_person_commsion}
+            onChange={(e) => setThird_person_commision(e.target.value)}
+          />
+        </div>
+        <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="fourth_person_commsion">4th Person Commission:</label>
+          <input
+
+            type="number"
+            id="fourth_person_commsion"
+            value={fourth_person_commsion}
+            onChange={(e) => setFourth_person_commision(e.target.value)}
+          />
+        </div> <div style={{ marginBottom: "16px" }}>
+          <label htmlFor="fifth_person_commsion">5th Person Commission:</label>
+          <input
+
+            type="number"
+            id="fifth_person_commsion"
+            value={fifth_person_commsion}
+            onChange={(e) => setFifth_person_commision(e.target.value)}
           />
         </div>
         <button
