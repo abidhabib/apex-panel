e1d014d0dd67802c322e0d53880e0afbda7156c4
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2023-09-10 03:44:53 +0500

    infos

--- src/components/withdrwaRequest/WithdrwaRequest.jsx
+++ src/components/withdrwaRequest/WithdrwaRequest.jsx
@@ -4,13 +4,19 @@ import {
   collection,
   getDocs,
   query,
-  where
+  where,
+  doc,
+  updateDoc,
+  serverTimestamp,
 } from "firebase/firestore";
 import { db } from "../../firebase";
+import { Toaster, toast } from "react-hot-toast";
+import { Button } from "@mui/material";
 
 const WithdrawRequest = () => {
   const [data, setData] = useState([]);
-  
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -25,19 +31,59 @@ const WithdrawRequest = () => {
           });
         });
         setData(withdrawals);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
+  const updateStatus = async (id) => {
+    try {
+      const withdrawalRef = doc(db, "withdrawals", id);
+      await updateDoc(withdrawalRef, {
+        status: "approved",
+        approvedTime: serverTimestamp(),
+      });
+
+      // Remove the approved row from the local data
+      setData((prevData) => prevData.filter((item) => item.id !== id));
+
+      // Show a toast using react-hot-toast
+      toast.success("Status updated to Approved");
+    } catch (error) {
+      console.error("Error updating status:", error);
+    }
+  };
+
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
+    {field:'Account_Number',headerName:'Account Number',width:150},
+    {field:'Bank_Name',headerName:'Bank Name',width:150},
+    {field:'Account_Title',headerName:'Account Name',width:150},
     { field: 'amount', headerName: 'Amount', width: 150 },
     { field: 'status', headerName: 'Status', width: 150 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => (
+<Button variant="contained" color="success" loading={isLoading}
+          onClick={() => updateStatus(params.row.id)}
+          disabled={params.row.status === "approved"}
+        >
+          Approve
+          </Button>
+      ),
+    },
   ];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="datatable">
@@ -49,6 +95,7 @@ const WithdrawRequest = () => {
         rowsPerPageOptions={[9]}
         checkboxSelection
       />
+      <Toaster />
     </div>
   );
 };
